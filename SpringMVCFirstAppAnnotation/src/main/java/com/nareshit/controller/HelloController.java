d7d23c764016f6541063c24981372568a7477e18
Saroj <sarojkumarmohanty@gmail.com>
2017-08-26 21:54:55 +0530

    my first git local commit

---
+++ SpringMVCFirstAppAnnotation/src/main/java/com/nareshit/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.nareshit.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nareshit.bean.User;
+import com.nareshit.service.HelloService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController{
+	@Autowired
+	private HelloService helloService;
+	
+	@RequestMapping(value="hello", method=RequestMethod.GET)
+	public ModelAndView sayHello(@ModelAttribute("user1") User user1) {
+		String targetView="/WEB-INF/pages/welcome.jsp";
+		ModelAndView model=new ModelAndView(targetView);
+		model.addObject("test","sushree");
+		return model;
+		
+		/*msg=helloService.sayHello(name);
+		return new ModelAndView(targetView,"msg",msg);*/
+	}
+
+}
